ba60cd716ef99222570d0f632970d6412aa06fa4
dto-simba <dto.simba@gmail.com>
2024-02-02 15:16:07 +0800

    add seiyan rewards and nft

--- krp-deployment/modules/nft/cw721_base_store_wasm.ts
+++ krp-deployment/modules/nft/cw721_base_store_wasm.ts
@@ -1,6 +1,5 @@
 import { WalletData } from "@/types";
 import { loadingWalletData } from "@/env_data.ts";
-import { ContractsDeployed, readDeployedContracts } from "@/modules";
 import { printChangeBalancesByWalletData, storeCodeByWalletData } from "@/common.ts";
 
 (async (): Promise<void> => {
@@ -10,6 +9,7 @@ import { printChangeBalancesByWalletData, storeCodeByWalletData } from "@/common
   let filePath = "./contracts/nft/cw721_base.wasm";
   let codeId = await storeCodeByWalletData(walletData, filePath, "", {});
   // atlantic-2 code 4755
+  // pacific-1 code 2244
   console.info(`\n  --- --- store code : cw721 base, codeId: ${codeId} --- ---`);
 
 
